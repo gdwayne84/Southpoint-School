14f3560aa20eb9af97049bca0f3f94dcb9ab17a0
Dwayne <garydwayne@gmail.com>
2025-09-11 21:00:36 +0800

    update homepage

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -224,7 +224,7 @@ const Home: React.FC = () => {
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <div className="text-center max-w-3xl mx-auto">
                 <h2 className="text-4xl font-extrabold text-gray-900 sm:text-5xl">
-                    The Southpoint Difference
+                    The Southpoint School Difference
                 </h2>
                 <p className="mt-4 text-xl text-gray-600">
                 At Southpoint School, education is a journey of both heart and mind. More than academic excellence, we nurture future-ready leaders with strong values and a love for lifelong learning.
@@ -278,7 +278,7 @@ const Home: React.FC = () => {
         </div>
       </section>
 
-      <section className="relative bg-black py-20 bg-cover bg-center" style={{backgroundImage: "url('/images/spsrandom8.jpg')" }}>
+      <section className="relative bg-black py-20 bg-cover bg-center" style={{backgroundImage: "url('images/spsrandom8.jpg')" }}>
        <div className="absolute inset-0 bg-battle-green/80"></div>
        <div className="relative container mx-auto px-4 sm:px-6 lg:px-8 text-center text-white">
            <h2 className="text-4xl sm:text-5xl font-extrabold">Ready to Begin Your Journey?</h2>
